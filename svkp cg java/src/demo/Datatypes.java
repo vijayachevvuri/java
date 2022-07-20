01dea80edde7f5679e6a9b0ad28a2844d55eeac3
acer <acer@TMP249-G2-M.bbrouter>
2022-07-20 12:24:44 +0530

    Datatypes

---
+++ svkp cg java/src/demo/Datatypes.java
@@ -0,0 +1,31 @@
+
+	package demo;
+
+	public class Datatypes {
+
+	//character//
+		public static void main(String[] args) {
+			char letter = 'v';
+			System.out.println(letter);
+	//Integer//
+		int a = 20;
+		int b = 10;
+		int c = a+b;
+	    System.out.println("value c:" +c);
+	//Float//
+	   float n =26f%5f;
+	   System.out.println(n);
+		
+	//Double//
+	double p = 46d%5d;
+	System.out.println(p);
+	 //Boolean//
+	boolean var = true;
+	char var1 = 50 ;
+	System.out.println("enter the value of var:" +var);	
+	 //string//
+	String var2 = new String("vijaya");
+	System.out.println(""+var2);
+	}}
+
+
